d354c69af71ec7da3f9278f6ad6edeb2e5f9350e
viandraagsatri <vagsatri@gmail.com>
2026-09-09 13:52:21 +0700

    Pertemuan 3: First Widget

---
+++ lib/column_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ColumnWidget extends StatelessWidget {
+  const ColumnWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget Column')),
+      body: const Column(
+        children: [
+          Text('Jurusan Ilmu Komputer'),
+          Text('FMIPA'),
+          Text('Universitas Lampung'),
+          Text('2026'),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/row_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget Row')),
+      body: Row(
+        children: const [
+          Text('Ilmu Komputer '),
+          Text('FMIPA '),
+          Text('Universitas Lampung '),
+          Text('2025 '),
+        ],
+      ),
+    );
+  }
+}
